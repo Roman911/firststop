64805d969b0dbd78eeaff5af101d4035ac0e2ebe
romanlysyk <roma-lysyk@ukr.net>
2025-04-08 09:55:56 +0300

    updated project

--- app/[locale]/accessories/page.tsx
+++ app/[locale]/accessories/page.tsx
@@ -4,7 +4,7 @@ import Title from '@/components/UI/Title';
 import Item from '@/components/Accessories/Item';
 
 async function getAllCategories() {
-	const res = await fetch(`${process.env.SERVER_URL}/api/AllCategories`, {
+	const res = await fetch(`${process.env.SERVER_URL}/api/getCategory/8`, {
 		method: 'GET',
 		headers: {
 			'Access-Control-Allow-Credentials': 'true',
@@ -15,7 +15,7 @@ async function getAllCategories() {
 }
 
 export default async function Accessories() {
-	const allCategories = await getAllCategories();
+	const categories = await getAllCategories();
 
 	const path = [
 		{
@@ -30,7 +30,7 @@ export default async function Accessories() {
 			<Breadcrumbs path={ path } />
 			<Title isMain={ true } title='accessories' translations={ true } className='mt-3 text-lg font-medium px-0 md:px-3 mb-6 md:mb-1' />
 			<div className='grid grid-cols-2 md:grid-cols-3 gap-6 mt-8'>
-				{ allCategories.map((category: { image: string | null, slug: string, category_id: number }, index: number) => (
+				{ categories.children.map((category: { image: string | null, slug: string, category_id: number }, index: number) => (
 					<Item key={ index } id={ category.category_id } href='accessories' image={ category.image } slug={ category.slug } />
 				)) }
 			</div>
